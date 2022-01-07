e57554582144194eb0257cec0f4fe1472534b345
emmastyler <emmastyler1998@gmail.com>
2022-01-07 10:06:09 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -15,9 +15,7 @@ app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '../client/build')));
 
-//real codes
 
  
 mongoose
